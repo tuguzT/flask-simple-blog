e4087c0fc805dc1d28f83b1f6f5feb43873f16c8
tuguzT <timurka.tugushev@gmail.com>
2022-01-22 20:34:53 +0300

    Add 404 Not Found error handler

--- simple_blog/routes.py
+++ simple_blog/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, abort
 
 from simple_blog import app
 from simple_blog.repository.model.user import User
@@ -11,6 +11,12 @@ def index():
 
 @app.route('/user/<name>')
 def user(name: str):
-    user: User = User.query.filter_by(name=name).one()
+    user: User = User.query.filter_by(name=name).one_or_none() or abort(404)
     print(user)
     return render_template('user.html', title=user.name, user=user)
+
+
+# noinspection PyUnusedLocal
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('not_found.html', title='404 Not Found'), 404
