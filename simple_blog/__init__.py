41e23865c5cc003ae7b42e2aef3c74446c69abb9
tuguzT <timurka.tugushev@gmail.com>
2022-01-24 23:14:11 +0300

    Configure `flask shell` command to import database and all models

--- simple_blog/__init__.py
+++ simple_blog/__init__.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 from . import routes, repository, login  # noqa: E402
+
+
+@app.shell_context_processor
+def make_shell_context():
+    from .repository import db
+    from .repository.model import User, Session, Post, DeletedPosts
+
+    return {'db': db, 'User': User, 'Session': Session, 'Post': Post, 'DeletedPosts': DeletedPosts, }
