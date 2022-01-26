ddb37dda53ce0ca393f219d3824cd4031b9dad5e
tuguzT <timurka.tugushev@gmail.com>
2022-01-26 21:43:08 +0300

    Fix error from SQLAlchemy `sqlalchemy.exc.NoSuchModuleError: Can't load plugin: sqlalchemy.dialects:postgres`

--- simple_blog/config.py
+++ simple_blog/config.py
@@ -5,5 +5,5 @@ from typing import Final
 class Config:
     SECRET_KEY: Final[str] = os.environ.get('SECRET_KEY')
 
-    SQLALCHEMY_DATABASE_URI: Final[str] = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI: Final[str] = os.environ.get('DATABASE_URL').replace('postgres', 'postgresql')
     SQLALCHEMY_TRACK_MODIFICATIONS: Final[bool] = False
